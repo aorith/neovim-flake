160bc8196e1b6c2d2e8a4da7088f9848fbde6878
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-04-15 12:37:56 +0200

    update lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -15,64 +15,83 @@ local function ls_status()
   return ""
 end
 
-local filename = {
-  "filename",
-  file_status = true,
-  newfile_status = true,
-  path = 1,
-  shorting_target = 40,
+local components = {
+  mode = {
+    "mode",
+    icons_enabled = true,
+    fmt = function(str)
+      return str:sub(1, 3)
+    end,
+  },
 
-  symbols = {
-    modified = "[+]",
-    readonly = "[RO]",
-    unnamed = "[No Name]",
-    newfile = "[New]",
+  ls_status = {
+    ls_status,
+  },
+
+  location = {
+    "%l:%c%V %P 0x%B",
+    padding = { left = 1, right = 1 },
+  },
+
+  filename = {
+    "filename",
+    file_status = true,
+    newfile_status = true,
+    path = 1,
+    shorting_target = 40,
+
+    symbols = {
+      modified = "[+]",
+      readonly = "[RO]",
+      unnamed = "[No Name]",
+      newfile = "[New]",
+    },
+  },
+
+  filetype = {
+    "filetype",
+    colored = true,
+    icon_only = true,
+    padding = { left = 1, right = 0 },
+  },
+
+  diagnostics = {
+    "diagnostics",
+    sources = { "nvim_lsp" },
+    symbols = { error = " ", warn = " ", info = " ", hint = " " },
+    on_click = function()
+      -- open quickfix list when clicking on the diagnostics lualine entry
+      vim.diagnostic.setqflist()
+    end,
   },
 }
-
-local filetype = {
-  "filetype",
-  colored = true,
-  icon_only = true,
-  padding = { left = 1, right = 0 },
-}
-
-local disabled_filetypes = { "dashboard", "lazy", "alpha", "neo-tree", "Trouble", "noice" }
 
 require("lualine").setup({
   options = {
     component_separators = "",
     section_separators = "",
     icons_enabled = true,
-    globalstatus = true,
-    disabled_filetypes = { statusline = disabled_filetypes, winbar = disabled_filetypes },
+    globalstatus = false,
   },
   extensions = { "neo-tree", "quickfix", "toggleterm", "trouble" },
 
   sections = {
-    lualine_a = {
-      "mode",
-    },
+    lualine_a = { components.mode },
     lualine_b = {},
-    lualine_c = {
-      filetype,
-      filename,
-    },
+    lualine_c = { components.filetype, components.filename, components.diagnostics },
 
-    lualine_x = {
-      { ls_status },
-      {
-        "diagnostics",
-        on_click = function()
-          -- open quickfix list when clicking on the diagnostics lualine entry
-          vim.diagnostic.setqflist()
-        end,
-      },
-    },
-    lualine_y = {
-      { "%l:%c%V %P 0x%B", padding = { left = 1, right = 1 } }, -- cursor location
-    },
+    lualine_x = { components.ls_status },
+    lualine_y = { components.location },
     lualine_z = {},
   },
-  inactive_sections = {},
+
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { components.filetype, components.filename },
+
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
 })
