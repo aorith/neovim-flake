478201ed144a5c1c48143a9976381b8b692307df
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-09-14 13:29:31 +0200

    disable providers in standard config

--- nvim/lua/aorith/init.lua
+++ nvim/lua/aorith/init.lua
@@ -26,6 +26,12 @@ return function()
     --
     -- nvim without nix
     --
+    vim.g.loaded_node_provider = 0
+    vim.g.loaded_perl_provider = 0
+    vim.g.loaded_python_provider = 0
+    vim.g.loaded_python3_provider = 0
+    vim.g.loaded_ruby_provider = 0
+
     require("aorith.mini_deps")()
   end
 end
