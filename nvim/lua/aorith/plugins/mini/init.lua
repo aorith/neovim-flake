0b28874650a1b122da8fb3e5a72b4ab99dc1ecd6
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-09-21 13:21:39 +0200

    typo

--- nvim/lua/aorith/plugins/mini/init.lua
+++ nvim/lua/aorith/plugins/mini/init.lua
@@ -22,4 +22,4 @@ require("aorith.plugins.mini.completion").setup()
 
 map("n", "<leader>q", function() MiniBufremove.delete() end, { desc = "Delete current buffer" })
 map("n", "<leader>z", function() MiniMisc.zoom() end, { desc = "Zoom window" })
-map("n", "<leader>go", function() MiniDiff.toggle_overlay() end, { desc = "Toggle diff overlay" })
+map("n", "<leader>go", function() MiniDiff.toggle_overlay(0) end, { desc = "Toggle diff overlay" })
