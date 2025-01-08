da305d3308e44be2384d3133f6b3304fda7e150d
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-01-08 10:50:20 +0100

    improve lsp info on statusline

--- nvim/lua/aorith/plugins/mini/statusline.lua
+++ nvim/lua/aorith/plugins/mini/statusline.lua
@@ -1,22 +1,5 @@
 local blocked_filetypes = { ["neo-tree"] = true }
 
-local function get_attached_clients()
-  local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
-  if #buf_clients == 0 then return "{}" end
-
-  local unique_buf_client_names = {}
-  for _, client in pairs(buf_clients) do
-    if not unique_buf_client_names[client.name] then unique_buf_client_names[client.name] = true end
-  end
-
-  local client_names = {}
-  for key in pairs(unique_buf_client_names) do
-    table.insert(client_names, key)
-  end
-
-  return "{" .. table.concat(client_names, " ") .. "}"
-end
-
 local M = {}
 
 M.setup = function()
@@ -39,7 +22,7 @@ M.setup = function()
         local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
         local location = MiniStatusline.section_location({ trunc_width = 75 })
 
-        local lsp_clients = get_attached_clients()
+        local lsp_clients = M.section_lsp({ trunc_width = 75 })
 
         return MiniStatusline.combine_groups({
           { hl = mode_hl, strings = { mode } },
@@ -63,6 +46,49 @@ M.setup = function()
       end,
     },
   })
+
+  -- Track LSP with autocommands
+  M.create_custom_autocommands()
+end
+
+M.attached_lsp = {}
+
+M.create_custom_autocommands = function()
+  local gr = vim.api.nvim_create_augroup("CustomMiniStatusline", {})
+
+  local au = function(event, pattern, callback, desc)
+    vim.api.nvim_create_autocmd(event, { group = gr, pattern = pattern, callback = callback, desc = desc })
+  end
+
+  -- Use `schedule_wrap()` because at `LspDetach` server is still present
+  local track_lsp = vim.schedule_wrap(function(data)
+    M.attached_lsp[data.buf] = M.compute_attached_lsp(data.buf)
+    vim.cmd("redrawstatus")
+  end)
+
+  au({ "LspAttach", "LspDetach" }, "*", track_lsp, "Track LSP clients")
+end
+
+M.compute_attached_lsp = function(buf_id)
+  local buf_clients = vim.lsp.buf_get_clients(buf_id)
+  if #buf_clients == 0 then return "{}" end
+
+  local unique_buf_client_names = {}
+  for _, client in pairs(buf_clients) do
+    if not unique_buf_client_names[client.name] then unique_buf_client_names[client.name] = true end
+  end
+
+  local client_names = {}
+  for key in pairs(unique_buf_client_names) do
+    table.insert(client_names, key)
+  end
+
+  return "{" .. table.concat(client_names, " ") .. "}"
+end
+
+M.section_lsp = function(args)
+  if MiniStatusline.is_truncated(args.trunc_width) then return "" end
+  return M.attached_lsp[vim.api.nvim_get_current_buf()] or "{}"
 end
 
 return M
