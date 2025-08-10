1ce6f856d34d62b0e7b0ec1209c9ee89a6ca9863
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-08-10 11:46:54 +0200

    stop snippet sessions on ESC

--- nvim/lua/aorith/plugins/mini/snippets.lua
+++ nvim/lua/aorith/plugins/mini/snippets.lua
@@ -9,3 +9,16 @@ require("mini.snippets").setup({
     gen_loader.from_lang(),
   },
 })
+
+-- Stop all sessions on Normal mode exit
+local make_stop = function()
+  local au_opts = { pattern = "*:n", once = true }
+  au_opts.callback = function()
+    while MiniSnippets.session.get() do
+      MiniSnippets.session.stop()
+    end
+  end
+  vim.api.nvim_create_autocmd("ModeChanged", au_opts)
+end
+local opts = { pattern = "MiniSnippetsSessionStart", callback = make_stop }
+vim.api.nvim_create_autocmd("User", opts)
