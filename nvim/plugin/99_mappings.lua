7bc049821f0fa6e65fe9f9628e0409f67ae642a5
Manuel Sanchez Pinar <aomanu@gmail.com>
2026-09-14 09:58:07 +0200

    Update

--- nvim/plugin/99_mappings.lua
+++ nvim/plugin/99_mappings.lua
@@ -33,7 +33,7 @@ Keymap({ '<C-Right>', '<Cmd>vertical resize +2<CR>', desc = 'Increase window wid
 Keymap({ '<esc>', '<Cmd>noh<CR><ESC>', mode = { 'i', 'n' }, desc = 'Escape and clear hlsearch' })
 
 -- Don't reset indent on '#', see :h smartindent
-Keymap({ '#', 'X#', mode = 'i' })
+Keymap({ '#', 'X#', mode = 'i' })
 
 -- buffers
 Leadermap({ '<TAB>', '<Cmd>bnext<CR>', silent = true, desc = 'Next buffer' })
